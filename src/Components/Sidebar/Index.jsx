bcbdd42030e24e68262709604cbe7e53b074756e
Victor Oliveira <xitornet.23@gmail.com>
2024-01-31 21:42:09 -0300

    *Build - init-sidebar

--- src/Components/Sidebar/Index.jsx
+++ src/Components/Sidebar/Index.jsx
@@ -1,8 +1,12 @@
-function Sidebar({children}) {
+function Sidebar({ children }) {
   return (
     <>
-      <div>aaaaaaaa</div>
-      <div> {children} </div>
+      <div className="flex flex-row h-screen w-screen bg-[#000000]">
+        <div className="flex flex-col bg-[#121212] h-screen">
+          <div className="font-medium text-lg p-4 text-green-500">CRUD > FrutaControl</div>
+        </div>
+        <div className="text-red-500"> {children} </div>
+      </div>
     </>
   );
 }
